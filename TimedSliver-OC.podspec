bf9fba4ce0f1923f7b1ebbdab6bca512352229fd
JK-King <zhang_jiankun521@163.com>
2018-07-23 15:31:48 +0800

    修改 设置提示框中 字体颜色

--- TimedSliver-OC.podspec
+++ TimedSliver-OC.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'TimedSliver-OC'
-  s.version          = '0.2.6'
+  s.version          = '0.2.7'
   s.summary          = 'TimedSliver-OC 常用类的分类'
 
 # This description is used to generate tags and improve search results.
--- TimedSliver-OC/Classes/UIAlertController+JKAlert.m
+++ TimedSliver-OC/Classes/UIAlertController+JKAlert.m
@@ -49,6 +49,7 @@
     if ([cancelAction valueForKey:@"titleTextColor"]) {
         [cancelAction setValue:cancelColor forKey:@"titleTextColor"];
     }
+    [alert addAction:cancelAction];
     UIAlertAction *okAction = [UIAlertAction actionWithTitle:confirmText style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
         if (confirm) {
             confirm();
@@ -58,7 +59,6 @@
     if ([okAction valueForKey:@"titleTextColor"]) {
         [okAction setValue:confirmColor forKey:@"titleTextColor"];
     }
-    [alert addAction:cancelAction];
     [alert addAction:okAction];
     [alert jk_show];
 }
